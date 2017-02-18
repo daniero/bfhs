90c51fe68963887f3d251691a81b2780119024f5
Daniel Rødskog <danielr@mesan.no>
2017-02-18 22:29:25 +0100

    Add Instruction data type and parser function

--- bfhs.hs
+++ bfhs.hs
@@ -2,19 +2,50 @@ import System.Environment
 import Data.List
 import Data.Char
 
-data State = Init { program :: String }
+data Instruction = Inc
+                 | Dec
+                 | MoveRight
+                 | MoveLeft
+                 | OpenLoop
+                 | LoopStart { end :: Int }
+                 | LoopEnd { start :: Int }
+                 | Input
+                 | Output
+                 deriving (Eq, Show)
+
+parse' :: String -> [Instruction] -> [Int] -> [Instruction]
+parse' "" insts [] = insts
+parse' "" _ _ = error "Unmatched '['"
+parse' ('+' : s) insts jumps = parse' s (insts ++ [Inc]) jumps
+parse' ('-' : s) insts jumps = parse' s (insts ++ [Dec]) jumps
+parse' ('>' : s) insts jumps = parse' s (insts ++ [MoveRight]) jumps
+parse' ('<' : s) insts jumps = parse' s (insts ++ [MoveLeft]) jumps
+parse' ('.' : s) insts jumps = parse' s (insts ++ [Input]) jumps
+parse' (',' : s) insts jumps = parse' s (insts ++ [Output]) jumps
+parse' ('[' : s) insts jumps = parse' s (insts ++ [OpenLoop]) (length insts : jumps)
+parse' (']' : s) insts (curr : jumps) = let index = length insts
+                                            before = take curr insts
+                                            after = drop (curr + 1) insts
+                                            replaceOpenLoop = before ++ (LoopStart index) : after
+                                         in parse' s (replaceOpenLoop ++ [LoopEnd curr]) jumps
+parse' (']' : _) _ [] = error "Unmatched ']'"
+parse' (_ : s) insts jumps = parse' s insts jumps
+
+parse :: String -> [Instruction]
+parse s = parse' s [] []
+
+data State = Init { program :: [Instruction] }
            | State { tape :: [Int],
                      dataPointer :: Int,
-                     program :: String,
-                     instructionPointer :: Int,
-                     jumpList :: [Int]
+                     program :: [Instruction],
+                     instructionPointer :: Int
                    }
            | Print State
            | Read State
            | Terminate deriving (Show)
 
 step :: State -> State
-step (Init program) = State [] 0 program 0 []
+step (Init program) = State [] 0 program 0
 -- TODO
 
 run :: State -> IO State
@@ -26,7 +57,7 @@ run (Print state) = do
                     return state
 
 -- TODO
-run (State _ _ _ _ _) = do
+run (State _ _ _ _) = do
                         return Terminate
 
 
@@ -34,5 +65,5 @@ main = do
   args <- getArgs
   content <- readFile $ args !! 0
 
-  let start = Init content
+  let start = Init $ parse content
   print $ step start
